b81f15d93dd337c4669647de180c2d4763f192bc
Anabia-113 <baiganabia113@gmail.com>
2024-01-22 16:29:48 +0500

    create

--- Tik-tac-toe game/script.js
+++ Tik-tac-toe game/script.js
@@ -1,19 +1,6 @@
              //tic-tac-toe game
-// let boxes = document.querySelectorAll(".box");
-// let resetBtn = document.querySelector("#reset-btn");
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
 
-// let turn0 = true;
+let turn0 = true;
 
-function two() {
-    return 2;
-}
-
-function one() {
-    return 1;
-}
-
-function calculate(initialValue, incrementValue) {
-    return initialValue() + incrementValue() + incrementValue();
-}
-
-console.log(calculate(two, one));
