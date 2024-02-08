fbb0337f0c225893969f7800048333c7321c9c49
Anabia-113 <baiganabia113@gmail.com>
2024-02-08 16:53:00 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -646,14 +646,43 @@
 
 //-------------------------------------------------conditional statements-----------------------------------------------------------------
 
-let a = prompt("Enter your age");
-a = parseInt(a);
-if(a >0){
-    alert("valid");
-}else{
-    alert("invalid age");
-}
+// let a = prompt("Enter your age");
+// a = parseInt(a);
+// if(a >0){
+//     alert("valid");
+// }else{
+//     alert("invalid age");
+// }
 
+
+let age = 30;
+console.log("you", age>18? "can vote": "cannot vote")
+
+
+//=====================switch statements========================
+// let expr = "grapes"
+// switch (expr) {
+//     case "grapes":
+//       console.log("Oranges are $0.59 a pound.");
+//       break;
+//     case "grapes":
+//       console.log("Apples are $0.32 a pound.");
+//       break;
+//     case "grapes":
+//       console.log("Bananas are $0.48 a pound.");
+//       break;
+//     case "Cherries":
+//       console.log("Cherries are $3.00 a pound.");
+//       break;
+    
+//     case "Papayas":
+//       console.log("Mangoes and papayas are $2.79 a pound.");
+//       break;
+//     default:
+//       console.log(`Sorry, we are out of ${expr}.`);
+//   }
+  
+//   console.log("Is there anything else you'd like?");
 
 
 
--- PracticeJavascript/tempCodeRunnerFile.js
+++ PracticeJavascript/tempCodeRunnerFile.js
@@ -1,1 +1,23 @@
-age
+let expr = "grapes"
+// switch (expr) {
+//     case "grapes":
+//       console.log("Oranges are $0.59 a pound.");
+//       break;
+//     case "grapes":
+//       console.log("Apples are $0.32 a pound.");
+//       break;
+//     case "grapes":
+//       console.log("Bananas are $0.48 a pound.");
+//       break;
+//     case "Cherries":
+//       console.log("Cherries are $3.00 a pound.");
+//       break;
+    
+//     case "Papayas":
+//       console.log("Mangoes and papayas are $2.79 a pound.");
+//       break;
+//     default:
+//       console.log(`Sorry, we are out of ${expr}.`);
+//   }
+  
+//   console.log("Is there anything else you'd like?");
