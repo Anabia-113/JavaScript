44682e5acaab42776a3813f5c1c9f653cfef6a1b
Anabia-113 <baiganabia113@gmail.com>
2024-02-26 14:11:21 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -1155,3 +1155,11 @@ while (runAgain) {
 }*/
 
 //question 4
+/*let number = prompt("Enter a number");
+number = parseInt(number);
+
+if(number > 4){
+    location.href = "https://google.com"
+}*/
+
+//question 5
