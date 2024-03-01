ee721ac57cc26cc8b45d0b0c34bc535628ac382b
Anabia-113 <baiganabia113@gmail.com>
2024-03-01 12:46:06 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -1599,19 +1599,26 @@ joke.innerHTML = jokes[index].joke;*/
 
 // console.log(document.body.textContent);
 
+
+
+
 //-------------------------------------------------Attribute methods--------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------------------------------------
-let first = document.getElementById("first");
-let a= first.getAttribute("class");
-console.log(a);
-console.log(first.hasAttribute("class"));
-console.log(first.hasAttribute("style"));
+// let first = document.getElementById("first");
+// let a= first.getAttribute("class");
+// console.log(a);
+// console.log(first.hasAttribute("class"));
+// console.log(first.hasAttribute("style"));
 // first.setAttribute("hidden", "true");
 // first.removeAttribute("class");
-console.log(first.attributes);
-console.log(first.dataset);
-console.log(first.dataset.game);
-console.log(first.dataset.player);
+// console.log(first.attributes);
+// console.log(first.dataset);
+// console.log(first.dataset.game);
+// console.log(first.dataset.player);
+
+
+//-------------------------------------------------HTML insertion methods---------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
 
 
 
