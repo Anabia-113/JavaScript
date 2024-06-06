566cb3a4156d2b9ebfaa743a8bfdc80ae6353162
Anabia-113 <baiganabia113@gmail.com>
2024-06-06 13:51:01 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -1633,7 +1633,8 @@ joke.innerHTML = jokes[index].joke;*/
 //-------------------------------------------------insert adjacent element/html---------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------------------------------------
 
-first.insertAdjacentHTML("beforebegin", <div class="test">beforebegin</div>);
-first.insertAdjacentHTML("beforeend", <div class="test">beforeend</div>);
-first.insertAdjacentHTML("afterbegin", <div class="test">afterbegin</div>);
-first.insertAdjacentHTML("afterend", <div class="test">afterend</div>);
+first.insertAdjacentHTML("beforebegin", '<div class="test">beforebegin</div>');
+first.insertAdjacentHTML("beforeend", '<div class="test">beforeend</div>');
+first.insertAdjacentHTML("afterbegin", '<div class="test">afterbegin</div>');
+first.insertAdjacentHTML("afterend", '<div class="test">afterend</div>');
+first.remove();
