4fb9fe344f60dc14ef06bdeacd4c2b249fb92e1d
Anabia-113 <baiganabia113@gmail.com>
2024-06-24 12:24:02 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -1742,8 +1742,8 @@ let p1 = new Promise((resolve, reject)=>{
 })
 let p2 = new Promise((resolve, reject)=>{
   setTimeout(()=>{
-    // resolve("value 2");
-    reject(new Error("error"))
+    resolve("value 2");
+    // reject(new Error("error"))
   },2000);
 
 })
@@ -1765,7 +1765,8 @@ let p3 = new Promise((resolve, reject)=>{
 // })
 
 // let promise_all = Promise.all([p1,p2,p3]);
-let promise_all = Promise.allSettled([p1,p2,p3]);
+// let promise_all = Promise.allSettled([p1,p2,p3]);
+let promise_all = Promise.race([p1,p2,p3]);
 promise_all.then((value)=>{
   console.log(value);
 })
