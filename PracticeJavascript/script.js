034d2142e3fd8458820fbead90691262c8695565
Anabia-113 <baiganabia113@gmail.com>
2024-07-19 14:58:23 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -1889,6 +1889,10 @@ let options = {
   }),
 
 }
+fetch('https://jsonplaceholder.typicode.com/posts',options)
+  
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 
 
 
