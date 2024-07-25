00de7e856385d02ac25e7f0265408c4728424d73
Anabia-113 <baiganabia113@gmail.com>
2024-07-25 10:54:37 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -1925,10 +1925,10 @@ console.log(document.cookie);
 //------------------------local storage and related methods-------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 
-let key = prompt("enter the key");
-let value = prompt("enter the value");
-localStorage.setItem(key, value);
-console.log(`the value at ${key} is ${localStorage.getItem(key)}`);
+// let key = prompt("enter the key");
+// let value = prompt("enter the value");
+// localStorage.setItem(key, value);
+// console.log(`the value at ${key} is ${localStorage.getItem(key)}`);
 //localstorage.removeItem
 //localstorage.clear()
 
