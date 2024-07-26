759db449ecf4060cbb1df85d3c904696b7b2f5fb
Anabia-113 <baiganabia113@gmail.com>
2024-07-26 14:51:09 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -1919,5 +1919,11 @@ joke.innerHTML = jokes[index].joke;*/
 //Question1
 let url = "https://kontests.net/api/v1/all";
 let response = fetch(url);
+response.then((v)=>{
+  return v.json;
+}).then((v)=>{
+  console.log(v);
+
+})
 cardContainer.innerHTML = "allcards";
 
