5d34afb848dc046d78f9ce3912c27b0bb8d55e4d
Anabia-113 <baiganabia113@gmail.com>
2024-08-06 12:51:18 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -2035,3 +2035,16 @@ joke.innerHTML = jokes[index].joke;*/
 //-----------------------Inheritance & extends Keyword in JavaScript------------------------------------------
 //------------------------------------------------------------------------------------------------------------
 
+class Animal{
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+
+  }
+  run(){
+    console.log(this.name + "is running");
+  }
+  shout(){
+    console.log(this.name + "is shouting");
+  }
+}
