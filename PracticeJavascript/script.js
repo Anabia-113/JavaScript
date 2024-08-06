97e29e49e49fef9c36b135b0ef823a58a2a03073
Anabia-113 <baiganabia113@gmail.com>
2024-08-06 13:22:02 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -2060,3 +2060,19 @@ joke.innerHTML = jokes[index].joke;*/
 
 //-----------------------Method overriding in JavaScript------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------
+class Employee{
+  login(){
+    console.log("Employee has logged in")
+  }
+  logout(){
+    console.log("Employee has logged out")
+  }
+  requestLeaves(leaves){
+    console.log(`Employee has requeated for ${leaves} leaves`)
+  }
+}
+class Programmer extends Employee{
+  requestCoffee(x){
+    console.log(`Employee has requested for ${x} coffee`)
+  }
+}
