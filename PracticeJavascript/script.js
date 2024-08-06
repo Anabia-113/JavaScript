9f18386a84086f1adb18b025431fb28a1a876f8c
Anabia-113 <baiganabia113@gmail.com>
2024-08-06 12:03:11 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -2021,11 +2021,11 @@ class railwayForm{
 }
 //create form for harry
 let  harryForm = new railwayForm;
-harryForm.fill("harry",1234)
+// harryForm.fill("harry",1234)    no need to fill form if constructor is initialized
 
 //create form for harry2
 let  harryForm2 = new railwayForm;
-harryForm2.fill("harry2",5678)
+// harryForm2.fill("harry2",5678)   no need to fill form if constructor is initialized
 
 harryForm.submit();
 harryForm2.submit();
