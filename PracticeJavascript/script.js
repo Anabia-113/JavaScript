3691a6bff19513d0630a5c15b8f9a2a03f61fb5a
Anabia-113 <baiganabia113@gmail.com>
2024-08-09 12:31:31 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -2187,6 +2187,10 @@ submit.addEventListener("click", (e)=>{
   let descc = desc.value
   localStorage.setItem("todo", JSON.stringify([titlec,descc]))
   console.log(e)
+  todo.innerHTML = `
+  <h1>${titlec}<h1/>
+  <p>${descc}<p/>
+  `
 })
 
 deleteBtn.addEventListener("click", (e)=>{
