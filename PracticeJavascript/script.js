276330e206338ab6d4bb27b346d6577c4e8a2b4e
Anabia-113 <baiganabia113@gmail.com>
2024-08-19 12:36:35 +0500

    create

--- PracticeJavascript/script.js
+++ PracticeJavascript/script.js
@@ -2242,3 +2242,19 @@ joke.innerHTML = jokes[index].joke;*/
 // console.log(sum(...x))
 
 //Question 3
+const a = async(text, n=2)=>{
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve(text)
+    },1000*n)
+  })
+
+}
+(
+  async()=>{
+    let text = await a('hello')
+    console.log(text)
+    text = await a('world')
+    console.log(text)
+  }
+)()
